273dcb4489e5515088d442d4578eba2b870754fc
lynkas <cat@moki.cat>
2022-03-10 11:21:37 +0800

    use env in storage bucket

--- pkg/base3/storage.go
+++ pkg/base3/storage.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/google/uuid"
+	"os"
 	"strings"
 )
 
@@ -99,7 +100,7 @@ func New(s3Config *aws.Config) *prototype {
 		client:     client,
 		uploader:   s3manager.NewUploader(newSession),
 		downloader: s3manager.NewDownloader(newSession),
-		bucketName: "develop",
+		bucketName: os.Getenv("STORAGE_BUCKET"),
 	}
 
 }
